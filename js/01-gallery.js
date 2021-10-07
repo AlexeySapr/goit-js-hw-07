365e85880b725052a3707e8082cdb35d2ba49809
Alexey Sapr <alexeysapr@gmail.com>
2021-10-07 17:25:42 +0300

    add task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,3 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
+
+const galleryConteiner = document.querySelector('.gallery');
+
+for (const { preview, original, description } of galleryItems) {
+	galleryConteiner.insertAdjacentHTML(
+		'beforeend',
+		`<div class="gallery__item">
+  			<a class="gallery__link" href="${original}">
+    			<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+  			</a>
+		</div>`
+	);
+}
