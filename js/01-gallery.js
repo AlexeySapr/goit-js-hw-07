f55037e9b138f75cd99239ceaad548038c161bc5
Alexey Sapr <alexeysapr@gmail.com>
2021-10-07 16:47:18 +0300

    add task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,27 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-// console.log(galleryItems);
 
 //Получаем ссылку на нужный элемент
 const galleryConteiner = document.querySelector('.gallery');
 
-//запрет действия по умолчанию для ссылки
+//Пустой объект, куда будет записываться ссылка на модальное окно
+let instance = {};
+
+//Обработчик нажатий на элемент галереи
 galleryConteiner.addEventListener('click', (event) => {
-	event.preventDefault();
+	event.preventDefault(); //запрет действия по умолчанию для ссылки
+
+	const bigImageUrl = event.target.getAttribute('data-source');
+
+	instance = basicLightbox.create(`<img src="${bigImageUrl}"/>`, { closable: false });
+
+	instance.show();
 });
 
-//Создание и рендер разметки
-// for (const { preview, original, description } of galleryItems) {
-// 	galleryConteiner.insertAdjacentHTML(
-// 		'beforeend',
-// 		`<div class="gallery__item">
-//   			<a class="gallery__link" href="${original}">
-//     			<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
-//   			</a>
-// 		</div>`
-// 	);
-// }
+//Обработчик нажания на клавишу Escape
+document.addEventListener('keydown', (event) => {
+	if (event.code === 'Escape') {
+		instance.close();
+	}
+});
 
 //создание разметки
 function createGallery(galleryItems) {
@@ -45,3 +46,16 @@ function createGallery(galleryItems) {
 
 //рендер разметки
 galleryConteiner.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+/***************** другой вариант создания и рендера разметки *******************/
+
+// for (const { preview, original, description } of galleryItems) {
+// 	galleryConteiner.insertAdjacentHTML(
+// 		'beforeend',
+// 		`<div class="gallery__item">
+//   			<a class="gallery__link" href="${original}">
+//     			<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+//   			</a>
+// 		</div>`
+// 	);
+// }
