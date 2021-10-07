35710676b973a2a4a2ef733491d9649f653ff110
Alexey Sapr <alexeysapr@gmail.com>
2021-10-07 14:43:08 +0300

    add and render markup for task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+//Получаем ссылку на нужный элемент
+const galleryConteiner = document.querySelector('.gallery');
+
+//запрет действия по умолчанию для ссылки
+galleryConteiner.addEventListener('click', (event) => {
+	event.preventDefault();
+});
+
+//Создание и рендер разметки
+// for (const { preview, original, description } of galleryItems) {
+// 	galleryConteiner.insertAdjacentHTML(
+// 		'beforeend',
+// 		`<div class="gallery__item">
+//   			<a class="gallery__link" href="${original}">
+//     			<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+//   			</a>
+// 		</div>`
+// 	);
+// }
+
+//создание разметки
+function createGallery(galleryItems) {
+	const markup = galleryItems
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
+						<a class="gallery__link" href="${original}">
+							<img
+							class="gallery__image"
+							src="${preview}"
+							data-source="${original}"
+							alt="${description}"
+							/>
+						</a>
+					</div>`;
+		})
+		.join('');
+
+	return markup;
+}
+
+//рендер разметки
+galleryConteiner.insertAdjacentHTML('beforeend', createGallery(galleryItems));
