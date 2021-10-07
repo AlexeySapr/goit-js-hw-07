7d2bf71a4c53bbeb884f26e4301652d3d43270d2
Alexey Sapr <alexeysapr@gmail.com>
2021-10-07 17:29:05 +0300

    change task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,13 +4,25 @@ console.log(galleryItems);
 
 const galleryConteiner = document.querySelector('.gallery');
 
-for (const { preview, original, description } of galleryItems) {
-	galleryConteiner.insertAdjacentHTML(
-		'beforeend',
-		`<div class="gallery__item">
-  			<a class="gallery__link" href="${original}">
-    			<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
-  			</a>
-		</div>`
-	);
+//создание разметки
+function createGallery(galleryItems) {
+	const markup = galleryItems
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
+						<a class="gallery__link" href="${original}">
+							<img
+							class="gallery__image"
+							src="${preview}"
+							data-source="${original}"
+							alt="${description}"
+							/>
+						</a>
+					</div>`;
+		})
+		.join('');
+
+	return markup;
 }
+
+//рендер разметки
+galleryConteiner.insertAdjacentHTML('beforeend', createGallery(galleryItems));
