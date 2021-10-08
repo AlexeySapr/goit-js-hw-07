a074110105a15af37ffb867769f4fb1d83c9de63
Alexey Sapr <alexeysapr@gmail.com>
2021-10-08 17:19:22 +0300

    update task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
-
+//Получаем ссылку на нужный элемент
 const galleryConteiner = document.querySelector('.gallery');
+
+//Пустой объект, куда будет записываться ссылка на модальное окно
+let instance = {};
+
+//Переменная-флаг "открыто ли модальное окно?"
+let isModalOpen = false;
+
+//Обработчик нажатий на элемент галереи
+galleryConteiner.addEventListener('click', (event) => {
+	event.preventDefault(); //запрет действия по умолчанию для ссылки
+
+	const bigImageUrl = event.target.getAttribute('data-source');
+
+	instance = basicLightbox.create(`<img src="${bigImageUrl}"/>`, { closable: true });
+
+	instance.show();
+
+	isModalOpen = instance.visible();
+});
+
+//Обработчик нажания на клавишу Escape
+document.addEventListener('keydown', (event) => {
+	if (isModalOpen) {
+		if (event.code === 'Escape') {
+			instance.close();
+		}
+	}
+});
 
 //создание разметки
 function createGallery(galleryItems) {
 	const markup = galleryItems
 		.map(({ preview, original, description }) => {
 			return `<div class="gallery__item">
-				<a class="gallery__link" href="${original}">
-					<img
-					class="gallery__image"
-					src="${preview}"
-					data-source="${original}"
-					alt="${description}"
-					/>
-				</a>
-			</div>`;
+						<a class="gallery__link" href="${original}">
+							<img
+							class="gallery__image"
+							src="${preview}"
+							data-source="${original}"
+							alt="${description}"
+							/>
+						</a>
+					</div>`;
 		})
 		.join('');
 
@@ -26,3 +53,16 @@ function createGallery(galleryItems) {
 
 //рендер разметки
 galleryConteiner.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+/***************** другой вариант создания и рендера разметки *******************/
+
+// for (const { preview, original, description } of galleryItems) {
+// 	galleryConteiner.insertAdjacentHTML(
+// 		'beforeend',
+// 		`<div class="gallery__item">
+//   			<a class="gallery__link" href="${original}">
+//     			<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+//   			</a>
+// 		</div>`
+// 	);
+// }
