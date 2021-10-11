9ece610de5a0adf13170b54ee3bf2f2191283a7a
Alexey Sapr <alexeysapr@gmail.com>
2021-10-11 20:38:44 +0300

    add task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+//Получаем ссылку на нужный элемент
+const galleryConteiner = document.querySelector("ul.gallery");
+// console.log(galleryConteiner);
+
+//создание разметки
+function createGallery(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`;
+    })
+    .join("");
+
+  return markup;
+}
+
+//рендер разметки
+galleryConteiner.innerHTML = createGallery(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
